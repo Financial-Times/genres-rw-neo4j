aa67b970dec00db3666a8231f898e959a6930414
Bernadette Varga <bernadette.varga@ft.com>
2016-06-09 14:43:27 +0300

    Add identifier nodes.

--- genres/genres_service.go
+++ genres/genres_service.go
@@ -2,7 +2,7 @@ package genres
 
 import (
 	"encoding/json"
-
+	"fmt"
 	"github.com/Financial-Times/neo-utils-go/neoutils"
 	"github.com/jmcvetta/neoism"
 )
@@ -23,16 +23,22 @@ func (s service) Initialise() error {
 		"Thing":   "uuid",
 		"Concept": "uuid",
 		"Classification": "uuid",
-		"Genre": "uuid"})
+		"Genre": "uuid",
+		"FactsetIdentifier": "value",
+		"TMEIdentifier":     "value",
+		"UPPIdentifier":     "value"})
 }
 
 func (s service) Read(uuid string) (interface{}, bool, error) {
 	results := []Genre{}
 
 	query := &neoism.CypherQuery{
-		Statement: `MATCH (n:Genre {uuid:{uuid}}) return n.uuid
-		as uuid, n.canonicalName as canonicalName,
-		n.tmeIdentifier as tmeIdentifier`,
+		Statement: `MATCH (n:Genre {uuid:{uuid}})
+OPTIONAL MATCH (upp:UPPIdentifier)-[:IDENTIFIES]->(n)
+OPTIONAL MATCH (fs:FactsetIdentifier)-[:IDENTIFIES]->(n)
+OPTIONAL MATCH (tme:TMEIdentifier)-[:IDENTIFIES]->(n)
+OPTIONAL MATCH (lei:LegalEntityIdentifier)-[:IDENTIFIES]->(n)
+return distinct n.uuid as uuid, n.prefLabel as prefLabel, labels(n) as types, {uuids:collect(distinct upp.value), TME:collect(distinct tme.value), factsetIdentifier:fs.value, leiCode:lei.value} as alternativeIdentifiers`,
 		Parameters: map[string]interface{}{
 			"uuid": uuid,
 		},
@@ -54,22 +60,20 @@ func (s service) Read(uuid string) (interface{}, bool, error) {
 
 func (s service) Write(thing interface{}) error {
 
-	sub := thing.(Genre)
+	genre := thing.(Genre)
 
-	params := map[string]interface{}{
-		"uuid": sub.UUID,
+	//cleanUP all the previous IDENTIFIERS referring to that uuid
+	deletePreviousIdentifiersQuery := &neoism.CypherQuery{
+		Statement: `MATCH (t:Thing {uuid:{uuid}})
+		OPTIONAL MATCH (t)<-[iden:IDENTIFIES]-(i)
+		DELETE iden, i`,
+		Parameters: map[string]interface{}{
+			"uuid": genre.UUID,
+		},
 	}
 
-	if sub.CanonicalName != "" {
-		params["canonicalName"] = sub.CanonicalName
-		params["prefLabel"] = sub.CanonicalName
-	}
-
-	if sub.TmeIdentifier != "" {
-		params["tmeIdentifier"] = sub.TmeIdentifier
-	}
-
-	query := &neoism.CypherQuery{
+	//create-update node for TOPIC
+	createTopicQuery := &neoism.CypherQuery{
 		Statement: `MERGE (n:Thing {uuid: {uuid}})
 					set n={allprops}
 					set n :Concept
@@ -77,29 +81,68 @@ func (s service) Write(thing interface{}) error {
 					set n :Genre
 		`,
 		Parameters: map[string]interface{}{
-			"uuid":     sub.UUID,
-			"allprops": params,
+			"uuid": genre.UUID,
+			"allprops": map[string]interface{}{
+				"uuid":      genre.UUID,
+				"prefLabel": genre.PrefLabel,
+			},
 		},
 	}
 
-	return s.cypherRunner.CypherBatch([]*neoism.CypherQuery{query})
+	queryBatch := []*neoism.CypherQuery{deletePreviousIdentifiersQuery, createTopicQuery}
 
+	//ADD all the IDENTIFIER nodes and IDENTIFIES relationships
+	if genre.AlternativeIdentifiers.FactsetIdentifier != "" {
+		factsetIdentifierQuery := createNewIdentifierQuery(genre.UUID, factsetIdentifierLabel, genre.AlternativeIdentifiers.FactsetIdentifier)
+		queryBatch = append(queryBatch, factsetIdentifierQuery)
+	}
+
+	if genre.AlternativeIdentifiers.LeiCode != "" {
+		leiCodeIdentifierQuery := createNewIdentifierQuery(genre.UUID, leiIdentifierLabel, genre.AlternativeIdentifiers.LeiCode)
+		queryBatch = append(queryBatch, leiCodeIdentifierQuery)
+	}
+
+	for _, alternativeUUID := range genre.AlternativeIdentifiers.TME {
+		alternativeIdentifierQuery := createNewIdentifierQuery(genre.UUID, tmeIdentifierLabel, alternativeUUID)
+		queryBatch = append(queryBatch, alternativeIdentifierQuery)
+	}
+
+	for _, alternativeUUID := range genre.AlternativeIdentifiers.UUIDS {
+		alternativeIdentifierQuery := createNewIdentifierQuery(genre.UUID, uppIdentifierLabel, alternativeUUID)
+		queryBatch = append(queryBatch, alternativeIdentifierQuery)
+	}
+	return s.cypherRunner.CypherBatch(queryBatch)
+
+}
+
+func createNewIdentifierQuery(uuid string, identifierLabel string, identifierValue string) *neoism.CypherQuery {
+	statementTemplate := fmt.Sprintf(`MERGE (t:Thing {uuid:{uuid}})
+					CREATE (i:Identifier {value:{value}})
+					MERGE (t)<-[:IDENTIFIES]-(i)
+					set i : %s `, identifierLabel)
+	query := &neoism.CypherQuery{
+		Statement: statementTemplate,
+		Parameters: map[string]interface{}{
+			"uuid":  uuid,
+			"value": identifierValue,
+		},
+	}
+	return query
 }
 
 func (s service) Delete(uuid string) (bool, error) {
 	clearNode := &neoism.CypherQuery{
 		Statement: `
 			MATCH (s:Thing {uuid: {uuid}})
+			OPTIONAL MATCH (t)<-[iden:IDENTIFIES]-(i:Identifier)
 			REMOVE s:Concept
 			REMOVE s:Classification
 			REMOVE s:Genre
-			SET s={props}
+			DELETE iden, i
+			SET s={uuid:{uuid}}
 		`,
 		Parameters: map[string]interface{}{
 			"uuid": uuid,
-			"props": map[string]interface{}{
-				"uuid": uuid,
-			},
 		},
 		IncludeStats: true,
 	}
